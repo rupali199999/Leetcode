7fd6e123d3c8db0b15e767183f6cb0b0a030ba1d
rupalisingh <86226634+rupali199999@users.noreply.github.com>
2024-04-03 14:45:56 +0530

    Time: 2 ms (43.02%) | Memory: 7.1 MB (94.95%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    int paths(int i,int j,int m, int n,vector<vector<int>>& dp)
-    {
-        //if any i or j goes out of bounds then it will return 0, you missed this and put && instead of ||
-        if(i>=m || j>=n)
-            return 0;
-        if(i==m-1 && j==n-1)
-            return 1;
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else            
-            return dp[i][j]=paths(i,j+1,m,n,dp)+paths(i+1,j,m,n,dp);  
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp (m,vector<int>(n,-1));
-        int count=paths(0,0,m,n,dp);
-        return count;
+        int N=m+n-2;
+        int r=m-1;
+        double ans=1;
+        for(int i=1;i<=r;i++)
+        {
+            ans = ans*(N-r+i)/(i);
+        }
+        return (int)ans;
     }
-}; 
+};
