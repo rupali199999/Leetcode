c790aa72ff7cf35e3561e093e59da58b87c502fe
rupalisingh <86226634+rupali199999@users.noreply.github.com>
2024-04-06 11:55:44 +0530

    Time: 22 ms (50.77%) | Memory: 13.6 MB (26.27%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -2,39 +2,32 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int open=0;
-        int sz=s.size();
-        unordered_set<int> remove_idx;
-        for(int i=0;i<sz;i++)
+        int n=s.size();
+        string temp="";
+        for(int i=0;i<n;i++)
         {
-            if(s[i]=='(')
-                open++;
-            else if(s[i]==')')
-            {
-                if(open>0)
-                    open--;
-                else
-                    remove_idx.insert(i);
-            }
-        }
-        int close=0;
-        for(int i=sz-1;i>=0;i--)
-        {
-            if(s[i]==')')
-                close++;
+            if(s[i]>='a' && s[i]<='z')
+                temp.push_back(s[i]);
             else if(s[i]=='(')
             {
-                if(close>0)
-                    close--;
-                else
-                    remove_idx.insert(i);
+                open++;
+                temp.push_back(s[i]);
+            }
+            else if(s[i]==')' && open>0)
+            {
+                open--;
+                temp.push_back(s[i]);
             }
         }
         string res="";
-        for(int i=0;i<sz;i++)
+        for(int i=temp.size()-1;i>=0;i--)
         {
-            if(remove_idx.find(i)==remove_idx.end())
-                res.push_back(s[i]);
+            if(open>0 && temp[i]=='(')
+                open--;
+            else
+                res.push_back(temp[i]);
         }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
