a8c86be05bcb502f0d1aa2625f37f7ac2f452388
rupalisingh <86226634+rupali199999@users.noreply.github.com>
2025-02-11 01:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (27.37%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    bool isDigit(char c){
-        if(c=='0' || c=='1' || c=='2' || c=='3' || c=='4' || c=='5' || c=='6' || c=='7' || c=='8' || c=='9')
-            return true;
-
-        return false;
-    }
     string clearDigits(string s) {
-        string s1="";
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(isDigit(s[i])){
-                int j=i-1;
-                while(j>=0 && isDigit(s[j])){
-                    j--;
-                }
-                s[j]='0';
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i]) && !st.empty()){
+                st.pop();
             }
+            else if(!isdigit(s[i]))
+                st.push(s[i]);
         }
-        for(int i=0;i<n;i++)
-        {
-            if(!isDigit(s[i]))
-                s1+=s[i];
+        string s1="";
+        while(!st.empty()){
+            s1+=st.top();
+            st.pop();
         }
+        reverse(s1.begin(),s1.end());
         return s1;
     }
 };
